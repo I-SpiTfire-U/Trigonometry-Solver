bb5a3afc7b2530d59baf13489506fc345703b9ed
Lex Hann <Ispitfireu.extra@outlook.com>
2024-04-23 15:33:34 -0400

    Completely changed the functions for solving isosceles triangles, removed unnecessery code, optimized a few things!

--- Trigonometry Solver/Program.cs
+++ Trigonometry Solver/Program.cs
@@ -10,37 +10,32 @@
             Console.Title = "Trigonometry Solver";
             while (true)
             {
-                Console.Clear();
-
                 double result;
-                byte type = Menu("Triangle Type:", "Right Angle", "Isosceles");
-                byte part = Menu("Solving For:", "Side", "Angle");
+                bool isRightAngle = Convert.ToBoolean(CreateMenu("Triangle Type:", "Right Angle", "Isosceles"));
+                bool isSolvingSide = Convert.ToBoolean(CreateMenu("Solving For:", "Side", "Angle"));
 
-                result = type == 0 ? RightAngle(part, Menu("Function:", "Sine", "Cosine", "Tangent")) : Isosceles(part, Menu("Function:", "Sine Law", "Cosine Law"));
+                result = isRightAngle == false ? RightAngle(isSolvingSide, CreateMenu("Function:", "Sine", "Cosine", "Tangent")) : Isosceles(isSolvingSide, CreateMenu("Function:", "Sine Law", "Cosine Law"));
 
                 Console.Write("Result: ");
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine(result);
-                Console.ForegroundColor = ConsoleColor.White;
+                WriteLineColor(ConsoleColor.DarkYellow, result);
                 _ = Console.ReadKey();
+                Console.Clear();
             }
         }
 
-        private static double RightAngle(byte part, byte function)
+        private static double RightAngle(bool isSolvingSide, byte function)
         {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("+-----------+-----------------+---------------------------------------+---------------+\n" +
-                              "| Sides:    | Angles:         | Finding Sides:                        | SOH = Opp/Hyp |\n" +
-                              "| --------- | --------------- | ------------------------------------- | CAH = Adj/Hyp |\n" +
-                              "| X = Angle | X = Top Side    | Reg = func(angle) * side  < [X/Value] | TOA = Opp/Adj |\n" +
-                              "| Z = Side  | Z = Bottom Side | Inv = side / func(angle)  < [Value/X] |               |\n" +
-                              "+-----------+-----------------+---------------------------------------+---------------+\n");
-            Console.ForegroundColor = ConsoleColor.White;
+            WriteLineColor(ConsoleColor.Magenta, "+-----------+-----------------+---------------------------------------+---------------+\n" +
+                                                 "| Sides:    | Angles:         | Finding Sides:                        | SOH = Opp/Hyp |\n" +
+                                                 "| --------- | --------------- | ------------------------------------- | CAH = Adj/Hyp |\n" +
+                                                 "| X = Angle | X = Top Side    | Reg = func(angle) * side  < [X/Value] | TOA = Opp/Adj |\n" +
+                                                 "| Z = Side  | Z = Bottom Side | Inv = side / func(angle)  < [Value/X] |               |\n" +
+                                                 "+-----------+-----------------+---------------------------------------+---------------+\n");
 
             float x = GetValidFloat("X");
             float z = GetValidFloat("Z");
 
-            if (part == 0)
+            if (isSolvingSide == false)
             {
                 Console.Write("Inverse [true/false] > ");
                 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -49,55 +44,58 @@
                     function += 4;
                 }
                 Console.ForegroundColor = ConsoleColor.White;
-                return CalcSideRightAngle(ConvertToDegrees(x), z, function);
+                return CalculateSide(ConvertToDegrees(x), z, function);
             }
-            return CalcAngleRightAngle(x, z, function) * DivideByPI;
+            return CalculateAngle(x, z, function) * DivideByPI;
         }
 
-        private static double Isosceles(byte part, byte function)
+        private static double Isosceles(bool isSolvingSide, byte function)
         {
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("+----------------+-----------------+------------------+-------------------+\n" +
-                              "| Sine Law Side: | Sine Law Angle: | Cosine Law Side: | Cosine Law Angle: |\n" +
-                              "| -------------- | --------------- | ---------------- | ----------------- |\n" +
-                              "| X = Angle A    | X = Angle A     | X = Side A       | X = Side A        |\n" +
-                              "| Y = Angle B    | Y = Side A      | Y = Side B       | Y = Side B        |\n" +
-                              "| Z = Side A     | Z = Side B      | Z = Angle C      | Z = Side C        |\n" +
-                              "+----------------+-----------------+------------------+-------------------+\n");
-            Console.ForegroundColor = ConsoleColor.White;
+            WriteLineColor(ConsoleColor.Magenta, "+----------------+-----------------+------------------+-------------------+\n" +
+                                                 "| Sine Law Side: | Sine Law Angle: | Cosine Law Side: | Cosine Law Angle: |\n" +
+                                                 "| -------------- | --------------- | ---------------- | ----------------- |\n" +
+                                                 "| X = Angle A    | X = Angle A     | X = Side A       | X = Side A        |\n" +
+                                                 "| Y = Angle B    | Y = Side A      | Y = Side B       | Y = Side B        |\n" +
+                                                 "| Z = Side A     | Z = Side B      | Z = Angle C      | Z = Side C        |\n" +
+                                                 "+----------------+-----------------+------------------+-------------------+\n");
 
             float x = GetValidFloat("X");
             float y = GetValidFloat("Y");
             float z = GetValidFloat("Z");
 
-            return part == 0 ? CalcSideIsosceles(x, y, z, function) : CalcAngleIsosceles(x, y, z, function);
+            return function == 0 ? SineLaw(x, y, z, isSolvingSide) : CosineLaw(x, y, z, isSolvingSide);
         }
 
-        private static double CalcSideIsosceles(float x, float y, float z, byte function)
+        private static double SineLaw(float x, float y, float z, bool isSolvingSide)
         {
-            if (function == 0)
+            return isSolvingSide == false
+                ? Math.Sin(ConvertToDegrees(y)) * z / Math.Sin(ConvertToDegrees(x))
+                : Math.Asin(Math.Sin(ConvertToDegrees(x)) / y * z) * DivideByPI;
+        }
+
+        private static double CosineLaw(float x, float y, float z, bool isSolvingSide)
+        {
+            return isSolvingSide == false
+                ? Math.Sqrt((x * x) + (y * y) - (2 * x * y * Math.Cos(ConvertToDegrees(z))))
+                : Math.Acos(((x * x) + (y * y) - (z * z)) / (2 * x * y)) * DivideByPI;
+        }
+
+        private static double CalculateSide(float angle, float side, byte function)
+        {
+            return function switch
             {
-                x = ConvertToDegrees(x);
-                y = ConvertToDegrees(y);
-                return Math.Sin(y) * z / Math.Sin(x);
-            }
-            z = ConvertToDegrees(z);
-            return Math.Sqrt((x * x) + (y * y) - (2 * x * y * Math.Cos(z)));
+                0 => side * Math.Sin(angle),
+                1 => side * Math.Cos(angle),
+                2 => side * Math.Tan(angle),
+                4 => side / Math.Sin(angle),
+                5 => side / Math.Cos(angle),
+                _ => side / Math.Tan(angle),
+            };
         }
 
-        private static float ConvertToDegrees(float value)
+        private static double CalculateAngle(float side1, float side2, byte function)
         {
-            return (float)(value * DivideBy180);
-        }
-
-        private static double CalcAngleIsosceles(float x, float y, float z, byte function)
-        {
-            if (function == 0)
-            {
-                x = ConvertToDegrees(x);
-                return Math.Asin(Math.Sin(x) / y * z) * DivideByPI;
-            }
-            return Math.Acos(((x * x) + (y * y) - (z * z)) / (2 * x * y)) * DivideByPI;
+            return function == 0 ? Math.Asin(side1 / side2) : function == 1 ? Math.Acos(side1 / side2) : Math.Atan(side1 / side2);
         }
 
         private static float GetValidFloat(string prompt)
@@ -111,31 +109,16 @@
                     Console.ForegroundColor = ConsoleColor.White;
                     return value;
                 }
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid Input!");
-                Console.ForegroundColor = ConsoleColor.White;
+                WriteLineColor(ConsoleColor.Red, "Invalid Input!");
             }
         }
 
-        private static double CalcSideRightAngle(float angle, float side, byte function)
+        private static float ConvertToDegrees(float value)
         {
-            return function switch
-            {
-                0 => side * Math.Sin(angle),
-                1 => side * Math.Cos(angle),
-                2 => side * Math.Tan(angle),
-                4 => side / Math.Sin(angle),
-                5 => side / Math.Cos(angle),
-                _ => side / Math.Tan(angle),
-            };
+            return (float)(value * DivideBy180);
         }
 
-        private static double CalcAngleRightAngle(float side1, float side2, byte function)
-        {
-            return function == 0 ? Math.Asin(side1 / side2) : function == 1 ? Math.Acos(side1 / side2) : Math.Atan(side1 / side2);
-        }
-
-        private static byte Menu(string title, params string[] options)
+        private static byte CreateMenu(string title, params string[] options)
         {
             byte length = (byte)options.Length;
 
@@ -145,9 +128,7 @@
                 for (int i = 0; i < length; i++)
                 {
                     Console.Write($"[{i}] ");
-                    Console.ForegroundColor = ConsoleColor.Green;
-                    Console.WriteLine(options[i]);
-                    Console.ForegroundColor = ConsoleColor.White;
+                    WriteLineColor(ConsoleColor.Green, options[i]);
                 }
 
                 Console.Write("> ");
@@ -158,12 +139,17 @@
                     Console.Clear();
                     return index;
                 }
-                Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Invalid option selected!");
-                Console.ForegroundColor = ConsoleColor.White;
+                WriteLineColor(ConsoleColor.Red, "Invalid Option Selected!");
                 _ = Console.ReadKey();
                 Console.Clear();
             }
+        }
+
+        private static void WriteLineColor(ConsoleColor color, object? value)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(value);
+            Console.ForegroundColor = ConsoleColor.White;
         }
     }
 }
